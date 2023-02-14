8057f3c4f569cea7ead217321edf6823d75b57f2
Simone Codarin <codarinsimone@gmail.com>
2023-02-14 18:35:32 +0100

    responsive

--- src/components/IconBar.vue
+++ src/components/IconBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex bg-white w-[390px] h-16 border-b-2 border-black mb-5 fixed xl:w-[1920px] xl:h-36 md:w-[834px] md:h-28">
+    <div class="flex bg-white w-[390px] h-16 border-b-2 border-black xl:w-[1920px] xl:h-36 md:w-[834px] md:h-28">
         <img src="../assets/image/logo-K.png" class="h-9 relative left-7 top-1 xl:h-24 xl:top-4 xl:left-12 md:h-16 md:left-28 md:top-4" @click="goHome" />
         <div v-if="cartStore.cart.items != 0">
             <img src="../assets/image/cart.png" class="h-8 m-1 relative left-[300px] top-1 xl:h-24 xl:top-3 xl:left-[1680px] md:h-[70px] md:left-[585px] md:top-3" @click="goCart"/>
