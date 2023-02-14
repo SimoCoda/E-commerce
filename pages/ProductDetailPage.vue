eab68449a70d0b475fc102f15edfa258c91b851c
Simone Codarin <codarinsimone@gmail.com>
2023-02-14 19:07:49 +0100

    responsive

--- pages/ProductDetailPage.vue
+++ pages/ProductDetailPage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="z-20 flex left-0 top-0">
+    <div class="z-20 left-0 top-0">
         <IconBar class="-mt-1 pt-3 z-50" /> 
     </div>
-    <div class="mt-10 md:mt-36 xl:mt-48">
+    <div class="mt-10 md:mt-12 xl:mt-12">
         <ProductInfo class="z-10"/>
     </div>
 
